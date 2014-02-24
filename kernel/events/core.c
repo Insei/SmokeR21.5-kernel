83f1f5e0723d81b7b80cb788ba3f130e7909b0ff
Peter Zijlstra <peterz@infradead.org>
2014-02-24 12:06:12 +0100

    UPSTREAM: perf: Fix hotplug splat
    (cherry picked from commit e3703f8cdfcf39c25c4338c3ad8e68891cca3731)
    
    Drew Richardson reported that he could make the kernel go *boom* when hotplugging
    while having perf events active.
    
    It turned out that when you have a group event, the code in
    __perf_event_exit_context() fails to remove the group siblings from
    the context.
    
    We then proceed with destroying and freeing the event, and when you
    re-plug the CPU and try and add another event to that CPU, things go
    *boom* because you've still got dead entries there.
    
    Reported-by: Drew Richardson <drew.richardson@arm.com>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Cc: Will Deacon <will.deacon@arm.com>
    Link: http://lkml.kernel.org/n/tip-k6v5wundvusvcseqj1si0oz0@git.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Change-Id: I99e2a7533ae3d5d789e7042dfb8048db97b5f1b0

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -7595,12 +7595,13 @@ static void __perf_event_exit_context(void *__info)
 {
 	struct remove_event re = { .detach_group = false };
 	struct perf_event_context *ctx = __info;
+	struct perf_event *event;
 
 	perf_pmu_rotate_stop(ctx->pmu);
 
 	rcu_read_lock();
-	list_for_each_entry_rcu(re.event, &ctx->event_list, event_entry)
-		__perf_remove_from_context(&re);
+	list_for_each_entry_rcu(event, &ctx->event_list, event_entry)
+		__perf_remove_from_context(event);
 	rcu_read_unlock();
 }
 
