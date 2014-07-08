3b9560e4ca85693d46407b5ed02a8125d5b82996
Marvin Tsai <marvin_tsai@htc.com>
2014-07-08 18:28:49 +0800

    driver: video: dc: bandwidth renegotiation may crash
    Add null pointer check in renegotiation callback.
    
    Change-Id: I14f3ba037b9755778920fa044c7a898194732972

--- drivers/video/tegra/tegra_adf.c
+++ drivers/video/tegra/tegra_adf.c
@@ -1161,9 +1161,15 @@ int tegra_adf_process_bandwidth_renegotiate(struct tegra_adf_info *adf_info,
 
 	event.base.type = TEGRA_ADF_EVENT_BANDWIDTH_RENEGOTIATE;
 	event.base.length = sizeof(event);
-	event.total_bw = bw->total_bw;
-	event.avail_bw = bw->avail_bw;
-	event.resvd_bw = bw->resvd_bw;
+	if (bw == NULL) {
+		event.total_bw = 0;
+		event.avail_bw = 0;
+		event.resvd_bw = 0;
+	} else {
+		event.total_bw = bw->total_bw;
+		event.avail_bw = bw->avail_bw;
+		event.resvd_bw = bw->resvd_bw;
+	}
 	return adf_event_notify(&adf_info->base.base, &event.base);
 }
 
