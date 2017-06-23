79881e4f90d5543647c30854bb127366da24e49d
Insei <goodmobiledevices@gmail.com>
2017-06-24 02:07:49 +0300

    arch: arm: mach-tegra: ardbeg: Remove dtv device, unused in mocha

--- arch/arm/mach-tegra/board-ardbeg.c
+++ arch/arm/mach-tegra/board-ardbeg.c
@@ -1292,16 +1292,6 @@ static void __init tegra_ardbeg_early_init(void)
 		tegra_soc_device_init("ardbeg");
 }
 
-static struct tegra_dtv_platform_data ardbeg_dtv_pdata = {
-	.dma_req_selector = 11,
-};
-
-static void __init ardbeg_dtv_init(void)
-{
-	tegra_dtv_device.dev.platform_data = &ardbeg_dtv_pdata;
-	platform_device_register(&tegra_dtv_device);
-}
-
 static struct tegra_io_dpd pexbias_io = {
 	.name			= "PEX_BIAS",
 	.io_dpd_reg_index	= 0,
@@ -1444,7 +1434,6 @@ static void __init tegra_ardbeg_late_init(void)
 		loki_regulator_init();
 	else
 		ardbeg_regulator_init();
-	ardbeg_dtv_init();
 	ardbeg_suspend_init();
 
 	if ((board_info.board_id == BOARD_PM374) ||
