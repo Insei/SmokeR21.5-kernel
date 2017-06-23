9341c7c9231a0263917873481e0829aa3c7a20ee
Insei <goodmobiledevices@gmail.com>
2017-06-23 11:26:57 +0300

    board: mocha: drivers: Add sensors * Add STMicroelectronics sensor-hub driver * Add AK09911 compass driver * Add BMP280 device driver

--- arch/arm/mach-tegra/board-ardbeg-sensors.c
+++ arch/arm/mach-tegra/board-ardbeg-sensors.c
@@ -63,95 +63,6 @@ static struct i2c_board_info ardbeg_i2c_board_info_cm32181[] = {
 };
 #endif
 
-/* MPU board file definition    */
-static struct mpu_platform_data mpu9250_gyro_data = {
-	.int_config     = 0x10,
-	.level_shifter  = 0,
-	/* Located in board_[platformname].h */
-	.orientation    = MPU_GYRO_ORIENTATION,
-	.sec_slave_type = SECONDARY_SLAVE_TYPE_NONE,
-	.key            = {0x4E, 0xCC, 0x7E, 0xEB, 0xF6, 0x1E, 0x35, 0x22,
-			0x00, 0x34, 0x0D, 0x65, 0x32, 0xE9, 0x94, 0x89},
-};
-
-static struct mpu_platform_data mpu9250_gyro_data_e1762 = {
-	.int_config     = 0x10,
-	.level_shifter  = 0,
-	/* Located in board_[platformname].h */
-	.orientation    = MPU_GYRO_ORIENTATION_E1762,
-	.sec_slave_type = SECONDARY_SLAVE_TYPE_NONE,
-	.key            = {0x4E, 0xCC, 0x7E, 0xEB, 0xF6, 0x1E, 0x35, 0x22,
-			0x00, 0x34, 0x0D, 0x65, 0x32, 0xE9, 0x94, 0x89},
-};
-
-static struct mpu_platform_data mpu_compass_data = {
-	.orientation    = MPU_COMPASS_ORIENTATION,
-	.config         = NVI_CONFIG_BOOT_MPU,
-};
-
-static struct mpu_platform_data mpu_bmp_pdata = {
-	.config         = NVI_CONFIG_BOOT_MPU,
-};
-
-static struct i2c_board_info __initdata inv_mpu9250_i2c0_board_info[] = {
-	{
-		I2C_BOARD_INFO(MPU_GYRO_NAME, MPU_GYRO_ADDR),
-		.platform_data = &mpu9250_gyro_data,
-	},
-	{
-		/* The actual BMP180 address is 0x77 but because this conflicts
-		 * with another device, this address is hacked so Linux will
-		 * call the driver.  The conflict is technically okay since the
-		 * BMP180 is behind the MPU.  Also, the BMP180 driver uses a
-		 * hard-coded address of 0x77 since it can't be changed anyway.
-		 */
-		I2C_BOARD_INFO(MPU_BMP_NAME, MPU_BMP_ADDR),
-		.platform_data = &mpu_bmp_pdata,
-	},
-	{
-		I2C_BOARD_INFO(MPU_COMPASS_NAME, MPU_COMPASS_ADDR),
-		.platform_data = &mpu_compass_data,
-	},
-};
-
-static void mpuirq_init(void)
-{
-	int ret = 0;
-	unsigned gyro_irq_gpio = MPU_GYRO_IRQ_GPIO;
-	unsigned gyro_bus_num = MPU_GYRO_BUS_NUM;
-	char *gyro_name = MPU_GYRO_NAME;
-	struct board_info board_info;
-
-	pr_info("*** MPU START *** mpuirq_init...\n");
-
-	tegra_get_board_info(&board_info);
-
-	ret = gpio_request(gyro_irq_gpio, gyro_name);
-	if (ret < 0) {
-		pr_err("%s: gpio_request failed %d\n", __func__, ret);
-		return;
-	}
-
-	ret = gpio_direction_input(gyro_irq_gpio);
-	if (ret < 0) {
-		pr_err("%s: gpio_direction_input failed %d\n", __func__, ret);
-		gpio_free(gyro_irq_gpio);
-		return;
-	}
-	pr_info("*** MPU END *** mpuirq_init...\n");
-
-	/* TN8 with diferent Compass address from ardbeg */
-	if (of_machine_is_compatible("nvidia,tn8"))
-		inv_mpu9250_i2c0_board_info[2].addr = MPU_COMPASS_ADDR_TN8;
-
-	if (board_info.board_id == BOARD_E1762)
-		inv_mpu9250_i2c0_board_info[0].platform_data =
-					&mpu9250_gyro_data_e1762;
-	inv_mpu9250_i2c0_board_info[0].irq = gpio_to_irq(MPU_GYRO_IRQ_GPIO);
-	i2c_register_board_info(gyro_bus_num, inv_mpu9250_i2c0_board_info,
-		ARRAY_SIZE(inv_mpu9250_i2c0_board_info));
-}
-
 /*
  * Soc Camera platform driver for testing
  */
@@ -2392,15 +2303,6 @@ int __init ardbeg_sensors_init(void)
 {
 	struct board_info board_info;
 	tegra_get_board_info(&board_info);
-	/* PM363 and PM359 don't have mpu 9250 mounted */
-	/* TN8 sensors use Device Tree */
-	if (board_info.board_id != BOARD_PM363 &&
-		board_info.board_id != BOARD_PM359 &&
-		!of_machine_is_compatible("nvidia,tn8") &&
-		!of_machine_is_compatible("nvidia,bowmore") &&
-		board_info.board_id != BOARD_PM375 &&
-		board_info.board_id != BOARD_PM377)
-		mpuirq_init();
 	ardbeg_camera_init();
 
 	if (board_info.board_id == BOARD_P1761 ||
