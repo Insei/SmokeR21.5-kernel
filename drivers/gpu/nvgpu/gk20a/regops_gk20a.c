9a48827c33f16b30ce46d285ce9d3adfad54bcd9
arttttt <artem-bambalov@yandex.ru>
2017-06-25 18:28:11 +0300

    drivers: gpu: remove unused variables

--- drivers/gpu/nvgpu/gk20a/regops_gk20a.c
+++ drivers/gpu/nvgpu/gk20a/regops_gk20a.c
@@ -341,13 +341,6 @@ static const u32 gk20a_runcontrol_whitelist[] = {
 static const u32 gk20a_runcontrol_whitelist_count =
 	ARRAY_SIZE(gk20a_runcontrol_whitelist);
 
-static const struct regop_offset_range gk20a_runcontrol_whitelist_ranges[] = {
-	{ 0x00419e10,   1 },
-	{ 0x0041c610,   1 },
-	{ 0x00501e10,   1 },
-	{ 0x00504610,   1 },
-};
-
 /* quad ctl */
 static const u32 gk20a_qctl_whitelist[] = {
 	0x00504670,
@@ -362,11 +355,6 @@ static const u32 gk20a_qctl_whitelist[] = {
 };
 static const u32 gk20a_qctl_whitelist_count =
 	ARRAY_SIZE(gk20a_qctl_whitelist);
-
-static const struct regop_offset_range gk20a_qctl_whitelist_ranges[] = {
-	{ 0x00504670,   1 },
-	{ 0x00504730,   4 },
-};
 
 static bool validate_reg_ops(struct dbg_session_gk20a *dbg_s,
 			     u32 *ctx_rd_count, u32 *ctx_wr_count,
